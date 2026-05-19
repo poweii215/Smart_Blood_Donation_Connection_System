2330bea6ea6bb9cd27a680d6d151f2d96e6c9e27
yenle12345 <leyen.201295@gmail.com>
2026-05-19 21:12:26 +0700

    Delete SBDCs directory

--- SBDCs/install_python_deps.js
+++
@@ -1,3 +0,0 @@
-import { spawnSync } from 'child_process';
-const res = spawnSync('pip3', ['install', 'fastapi', 'uvicorn', 'python-jose[cryptography]', 'passlib[bcrypt]', 'pydantic'], { encoding: 'utf8' });
-console.log(res.stdout || res.stderr);
